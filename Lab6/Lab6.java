b27e1a3757df54b0e59434358496065ce5880358
Vishva22122121 <118505178+Vishva22122121@users.noreply.github.com>
2023-04-05 19:02:30 +0530

    comments

---
+++ Lab6/Lab6.java
@@ -0,0 +1,24 @@
+package 22122121-MDS273L-JAVA.Lab6;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Lab6 {
+    public static void main(String[] args) throws IOException{
+        File f = new File("C:\\Users\\HP\\Downloads\\text.txt");
+        try{
+            Lab6 fir = new Lab6(f);
+            int c;
+            int account = 0;
+            int eccount = 0;
+            int iccount = 0;
+            int occount = 0;
+            int uccount = 0;
+            while((c=fir read()!=-1)){
+                char ch =(char)c;
+
+            }
+        }
+    }
+    
+}
